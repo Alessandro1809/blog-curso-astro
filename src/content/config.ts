0907d0ac300802266aac93063700416c8464c6da
Alessandro1809 <dalessandro18dg@gmail.com>
2024-10-24 16:21:02 -0600

    optimizando imagenes de los posts con  el Image component

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,14 @@ import {z, defineCollection} from 'astro:content'//Usar zod para validacion de t
 
 const blogCollection = defineCollection({
     type: 'content',//content es para contenido que se puede renderizar y data es para metadata
-    schema: z.object({
+    schema: ({image})=> z.object({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        image: z.string(), 
+        image: image().refine(img=>img.width > 200 && img.height > 200, {
+            message: 'La imagen debe tener una anchura y altura de al menos 200px',
+           
+        }), 
 
 
 
