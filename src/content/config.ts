0a837cc4833eda4cefa51a7a68b46af9f2ee5816
Alessandro1809 <dalessandro18dg@gmail.com>
2024-10-30 23:37:23 -0600

    finalizando el  proyecto

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import {z, defineCollection} from 'astro:content'//Usar zod para validacion de tipado
+import {z, defineCollection, reference} from 'astro:content'//Usar zod para validacion de tipado
 
 const blogCollection = defineCollection({
     type: 'content',//content es para contenido que se puede renderizar y data es para metadata
@@ -6,7 +6,7 @@ const blogCollection = defineCollection({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        image: image().refine(img=>img.width > 200 && img.height > 200, {
+        image: image().refine(img=>img.width > 500 && img.height > 500, {
             message: 'La imagen debe tener una anchura y altura de al menos 200px',
            
         }), 
@@ -14,12 +14,27 @@ const blogCollection = defineCollection({
 
 
         //relaciones
-        author: z.string(),  
+        // author: z.string(),  
+        author: reference('author'),
         //relaciones   
         tags: z.array(z.string()), 
+
+        isDraft: z.boolean().default(false)
     })
 })
 
+
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({image})=>  z.object({
+        name: z.string(),
+        avatar: image()
+    })
+})
+
+
+
 export const collections = {
-    blog: blogCollection
+    blog: blogCollection,
+    author: authorCollection
 }
